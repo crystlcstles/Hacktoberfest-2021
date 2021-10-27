adb1cc406d2ac4494a6c8cf78dad138863d7691c
Boogeyman1234 <93274059+Boogeyman1234@users.noreply.github.com>
2021-10-27 09:20:11 -0700

    Create Ucase Lcase.py

---
+++ Python/Ucase Lcase.py
@@ -0,0 +1,6 @@
+a = input("Please Enter Your Own Character : ")
+
+if(a.isupper()):
+    print("The Given Character ", a, "is an Uppercase Alphabet")
+else:
+    print("The Given Character ", a, "is  a lowercase Alphabet")
